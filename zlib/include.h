6e70defb261ec1da9e668ebf8bbc85c80dee6069
Unknown <developer@appysphere.com>
2018-02-08 10:12:53 +0700

    Updated Podspec

---
+++ zlib/include.h
@@ -0,0 +1,5 @@
+#include <AppySDK.h>
+#include <Libraries/Bticino/OpenWebNetUtils.h>
+#include <Libraries/CocoaAsyncSocket/GCDAsyncSocket.h>
+#include <Libraries/CocoaAsyncSocket/GCDAsyncUdpSocket.h>
+#include <Libraries/Socket/SocketHandler.h>
