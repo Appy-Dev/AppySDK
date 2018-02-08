97d2f814ec196164a349462d388124f63053c211
Unknown <developer@appysphere.com>
2018-02-08 14:14:26 +0700

    Updated Framework

--- zlib/include.h
+++
@@ -1,5 +0,0 @@
-#include <AppySDK.h>
-#include <Libraries/Bticino/OpenWebNetUtils.h>
-#include <Libraries/CocoaAsyncSocket/GCDAsyncSocket.h>
-#include <Libraries/CocoaAsyncSocket/GCDAsyncUdpSocket.h>
-#include <Libraries/Socket/SocketHandler.h>
